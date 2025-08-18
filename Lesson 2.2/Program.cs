e11128bc8d763413f25d1af6358262f30c6156ac
Andrey Korochkin <charpdeveloper@outlook.com>
2025-08-18 20:13:06 +0300

    Добавлены уроки 2.2, 2.3

---
+++ Lesson 2.2/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.ComponentModel.Design;
+/*
+int num = Int32.Parse(Console.ReadLine()); // Запрашиваем ввод из консоли и вытягиваем из него число
+
+if ((num%2)==0) // Напишите свой код вместо "..." слева
+{
+    Console.WriteLine("EVEN");
+}
+else
+{
+    Console.WriteLine("ODD");
+}
+*/
+//Напишите программу, в которую пользователь вводит с клавиатуры два числа. А программа сравнивает два введенных числа и выводит на консоль результат сравнения.
+/*
+string line = Console.ReadLine();
+string[] splitString = line.Split(' ');
+
+int a = Convert.ToInt32(splitString[0]);
+int b = Convert.ToInt32(splitString[1]);
+
+if (a==b)
+{
+    Console.WriteLine("a = b");
+}
+else if(a<b)
+{
+    Console.WriteLine("a < b");
+}
+else
+{
+    Console.WriteLine("a > b");
+}
+*/
+//Напишите программу, в которой пользователь вводит с клавиатуры три числа.
+//Программа должна вывести минимальное значение из представленных.
+//При написании кода учитывайте, что числа могут быть одинаковыми.
+//Необходимо писать общее решение для всех случаев
+
+string line = Console.ReadLine();
+string[] splitString = line.Split(' ');
+
+int a = Convert.ToInt32(splitString[0]);
+int b = Convert.ToInt32(splitString[1]);
+int c = Convert.ToInt32(splitString[2]);
+/*
+int minValue = 0;
+if ((a != 0) && (b != 0) && (c !=0 ))
+{
+    if (((a != b) && (b != c) && (a != c)))
+    {
+        if (a < b && b < c)
+        {
+            minValue = a;
+        }
+        else
+        {
+            if (a < b && c < a)
+            {
+                minValue = c;
+            }
+            else if (b < a && b < c)
+            {
+                minValue = b;
+            }
+            else
+            {
+                minValue = c;
+            }
+        }
+    }
+    else if (a == b && c < b)
+    {
+        minValue = c;
+    }
+    else if (b == c && a < c)
+    { minValue = a; }
+    else if (a == b && b == c)
+    {
+        minValue = c;
+    }
+    else { minValue = b; }
+
+}
+else 
+{ minValue = 0; }
+Console.WriteLine(minValue);
+*/
+int minValue = (a <= b && a <= c) ? a : (b <= a && b <= c) ? b : c;
+
+Console.WriteLine(minValue);
---
+++ Lesson 2.3/Program.cs
@@ -0,0 +1,277 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+using System.ComponentModel.Design;
+/*
+char letter = Convert.ToChar(Console.ReadLine());
+switch (letter)
+{
+    case 'а':
+    case 'о':
+    case 'у':
+    case 'ы':
+    case 'э':
+    case 'я':
+    case 'ё':
+    case 'ю':
+    case 'и':
+    case 'е':
+        Console.WriteLine("гласная");
+        break;
+    default:
+        break;
+}
+
+/*Напишите программу, в который пользователь вводит цифру от 1 до 31. 
+    То есть день месяца.  Учитывая что 1 число, это понедельник, программа должна выводить какой сегодня день недели. 
+*/
+/*
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+int x = dayNumber % 7;
+if (x == 1)
+{
+    switch (x)
+    {
+        case 1:
+            Console.WriteLine("Понедельник");
+            break;
+    }
+}
+else if (x == 2)
+{
+    switch (x)
+    {
+        case 2:
+            Console.WriteLine("Вторник");
+            break;
+    }
+}
+else if (x == 3)
+{
+    switch (x)
+    {
+        case 3:
+            Console.WriteLine("Среда");
+            break;
+    }
+}
+else if (x == 4)
+{
+    switch (x)
+    {
+        case 4:
+            Console.WriteLine("Четверг");
+            break;
+    }
+}
+else if (x == 5)
+{
+    switch (x)
+    {
+        case 5:
+            Console.WriteLine("Пятница");
+            break;
+    } }
+else if (x == 6)
+{
+    switch (x)
+    {
+        case 6:
+            Console.WriteLine("Суббота");
+            break;
+    }
+}
+else if (x == 0)
+{
+    switch (x)
+    {
+        case 0:
+            Console.WriteLine("Воскресенье");
+            break;
+    }
+}
+
+*/
+//Напишите программу, которая выводит пользователю сообщение "Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение".
+/*
+Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение");
+int operation = Convert.ToInt32(Console.ReadLine());
+
+switch (operation)
+{
+    case 1:
+        Console.WriteLine("Сложение");
+        break;
+    case 2:
+        Console.WriteLine("Вычитание");
+        break;
+    case 3:
+        Console.WriteLine("Умножение");
+        break;
+    default:
+        Console.WriteLine("Неизвестная операция!");
+        break;
+}
+
+*/
+/*
+Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение");
+
+string line = Console.ReadLine();
+string[] splitString = line.Split(' ');
+
+int operation = Convert.ToInt32(splitString[0]);
+double a = Convert.ToDouble(splitString[1]);
+double b = Convert.ToDouble(splitString[2]);
+double result = 0;
+
+switch (operation)
+{
+    case 1:
+        result = a + b;
+        Console.WriteLine("Результат операции " + result);
+        break;
+    case 2:
+        result = a - b;
+        Console.WriteLine("Результат операции " + result);
+        break;
+    case 3:
+        result = a * b;
+        Console.WriteLine("Результат операции " + result);
+        break;
+    default:
+        Console.WriteLine("Неизвестная операция!");
+        break;
+}
+*/
+/*
+Сыграем в "камень - ножницы - бумага"?
+
+В этой программе в консоль вводятся два числа, каждое с новой строки
+
+Первое число (сверху) - выбор первого игрока, второе число (снизу) - выбор второго
+
+0 - это камень
+
+1 - это ножницы
+
+2 - это бумага
+
+Допишите программу, чтобы она выводила в консоль информацию о победителе одним из следующих вариантов:
+*/
+/*
+string[] signs = { "камень", "ножницы", "бумага" };
+Console.WriteLine("Выбирает первый игрок: 0 - камень, 1 - ножницы, 2 - бумага");
+int input1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Первый игрок выбрал: {signs[input1]}");
+Console.WriteLine("Выбирает второй игрок: 0 - камень, 1 - ножницы, 2 - бумага");
+int input2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Второй игрок выбрал: {signs[input2]}");
+if (((input1 == 0) && (input2 == 1)) || ((input1 == 1) && (input2 == 2)) || ((input1 == 2) && (input2 == 0)))
+{
+    input1 = 0;
+    switch (input1)
+    {
+        case 0:
+            Console.WriteLine("Результат: Победил первый игрок");
+            break;
+    }
+}
+else if (((input1 == 1) && (input2 == 0)) || ((input1 == 2) && (input2 == 1)) || ((input1 == 0) && (input2 == 2)))
+{
+    input1 = 0;
+    switch (input1)
+    {
+        case 0:
+            Console.WriteLine("Результат: Победил второй игрок");
+            break;
+    }
+}
+else if (((input1 == 0) && (input2 == 0)) || ((input1 == 1) && (input2 == 1)) || ((input1 == 2) && (input2 == 2)))
+{
+    Console.WriteLine("Результат: Ничья");
+}
+
+else
+{
+
+    Console.WriteLine("Неизвестная операция!");
+}
+
+
+string[] signs = { "камень", "ножницы", "бумага" };
+Console.WriteLine("Выбирает первый игрок: 0 - камень, 1 - ножницы, 2 - бумага");
+int input1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Первый игрок выбрал: {signs[input1]}");
+Console.WriteLine("Выбирает второй игрок: 0 - камень, 1 - ножницы, 2 - бумага");
+int input2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Второй игрок выбрал: {signs[input2]}");
+
+switch (input1)
+{
+    case 0: //первый игрок выбрал камень
+     switch (input2)
+        {
+            case 0: Console.WriteLine("Результат: Ничья");break;
+            case 1: Console.WriteLine("Результат: Победил первый игрок");break;
+            case 2: Console.WriteLine("Результат: Победил второй игрок");break;
+
+        }
+        break;
+    case 1: //первый игрок выбрал ножницы
+        switch (input2)
+        {
+            case 0: Console.WriteLine("Результат: Победил второй игрок"); break;
+            case 1: Console.WriteLine("Результат: Ничья"); break;
+            case 2: Console.WriteLine("Результат: Победил первый игрок"); break;
+
+        }
+        break;
+    case 2: //первый игрок выбрал бумага
+        switch (input2)
+        {
+            case 0: Console.WriteLine("Результат: Победил первый игрок"); break;
+            case 1: Console.WriteLine("Результат: Победил второй игрок"); break;
+            case 2: Console.WriteLine("Результат: Ничья"); break;
+
+        }
+        break;
+}
+
+Дополните код чтобы он мог определять чётное или нечётное число было введено и выводило соответсвующие строки:
+"ODD" (нечётное) или "EVEN" (чётное).
+
+
+int num = Int32.Parse(Console.ReadLine()); // Запрашиваем ввод из консоли и вытягиваем из него число
+
+string answer = num % 2 == 0 ? "EVEN" : "ODD";
+
+Console.WriteLine(answer);
+
+*/
+
+/*Напишите программу для изменения координат точки посредством введения пользователем текстовых команд в консоль. 
+    Перемещение по оси y осуществляется командами вниз и вверх, а по оси x - командами влево и вправо.
+
+
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int x = 0;
+        int y = 0;
+
+        string arrow = Convert.ToString(Console.ReadLine());
+        switch (arrow)
+        {
+            case "вверх": y = y++;break;
+            case "вниз": y = y--; break;
+            case "влево": x = x--; break;
+            case "вправо": x = x++; break;
+        }
+
+    }
+}
+*/
