1132ce3503f30df577818402b31f9ebda5cf69b5
Andrey Korochkin <charpdeveloper@outlook.com>
2025-08-18 18:56:03 +0300

    Созданы решения для части уроков курса степика

---
+++ lesson 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+short x = 150;
+byte y = (byte)(x + x);
+Console.WriteLine(y);
+/*
+char symbol = '5';
+int number = 2;
+int sum = symbol + number;
+Console.WriteLine(sum);
+string x = "O";
+    string y = "lolo";
+Console.WriteLine(x + y);
+Console.WriteLine("Hello, World!");
+
+ int count = 10;
+double number = 7;
+string text = Console.ReadLine();
+double number2 = double.Parse(text);
+count = (int)number;//true
+number = count;//true
+Console.WriteLine(count);
+Console.WriteLine(number);
+Console.WriteLine(number2);*/
+
+
