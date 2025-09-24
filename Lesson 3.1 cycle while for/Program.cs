7a48c270935a33d8072641ee440fe73e54166f65
blikon <blikon@live.com>
2025-09-24 22:11:00 +0300

    Добавлены 2 задачи

---
+++ Lesson 3.1 cycle while for/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+/* Простая задачка для закрепления материала. 
+ * Пользователь вводит в консоль число. Выведите на экран приветственное сообщение
+ * "Hello, NoobCoders!" несколько раз, в зависимости от того, 
+ * какое число было введено пользователем.
+ 
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine("Hello, NoobCoders!");
+}
+*/
+/* В консоль пользователем вводится целое положительно число.
+ * Используя цикл while, напишите программу, которая выводит на экран
+ * через пробел числа от нуля до введенного пользователем числа включительно.
+ */
+/*
+  int i=0;
+        int num = int.Parse(Console.ReadLine());
+while (i <= num)
+
+        {
+
+            Console.Write(i);
+
+            if (i < num)
+
+            {
+
+                Console.Write(" "); // Добавляем пробел, если это не последнее число
+
+            }
+
+            i++;
+
+        }
+
+        Console.WriteLine();
+ */
+//добавлены 2 задачи
