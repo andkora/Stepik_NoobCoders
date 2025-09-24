48e48cae0774a94fba9cd5504eca6ab0953e8987
Andrey Korochkin <charpdeveloper@outlook.com>
2025-09-24 22:34:23 +0300

    Добавлена 3я и 4я задачи

--- Lesson 3.1 cycle while for/Program.cs
+++ Lesson 3.1 cycle while for/Program.cs
@@ -42,3 +42,42 @@ while (i <= num)
         Console.WriteLine();
  */
 //добавлены 2 задачи
+/*
+Теперь пользователь вводит целое положительное число, 
+    но от вас требуется вывести на экран все числа от нуля столбиком,
+    чтобы при этом сумма всех чисел в столбце была строго меньше введенного пользователем числа.
+     */
+/*
+int i = 0;
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+do
+
+{
+    // if (sum > num)
+    //{ break; }
+    Console.WriteLine(i);
+    // Console.Write(" ");
+    sum = sum + i;
+    // Console.WriteLine(sum);
+    i++;
+}
+while (sum < (num - i));
+
+
+/* Факториалом натурального числа n называют произведение всех натуральных чисел от 1 до n включительно.
+ * Напишите программу для вычисления факториала введенного пользователем числа.
+  */
+
+int num = int.Parse(Console.ReadLine());
+long fac=1;
+
+for (int i = 2; i <= num; i++) 
+{
+    fac = fac * i;
+    Console.Write("i= "+i);
+    Console.Write("  ");
+    Console.WriteLine("fac= "+fac);
+}
+
+//Добавлена 3я и 4я задачи
