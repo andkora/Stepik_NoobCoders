89c262c5460c9036badc2810beec6db73b39ed7f
Andrey Korochkin <charpdeveloper@outlook.com>
2025-09-26 00:33:39 +0300

    Попытка решить 5 задачу

--- Lesson 3.1 cycle while for/Program.cs
+++ Lesson 3.1 cycle while for/Program.cs
@@ -68,7 +68,7 @@ while (sum < (num - i));
 /* Факториалом натурального числа n называют произведение всех натуральных чисел от 1 до n включительно.
  * Напишите программу для вычисления факториала введенного пользователем числа.
   */
-
+/*
 int num = int.Parse(Console.ReadLine());
 long fac=1;
 
@@ -79,5 +79,85 @@ for (int i = 2; i <= num; i++)
     Console.Write("  ");
     Console.WriteLine("fac= "+fac);
 }
-
+*/
 //Добавлена 3я и 4я задачи
+//Задача 5
+/* Напишите программу, которая считает сумму всех простых чисел в определенном диапазоне
+ */
+int startValue = int.Parse(Console.ReadLine());
+int endValue = int.Parse(Console.ReadLine());
+
+//int startNumber = startValue < 0 ? -startValue : startValue;
+
+//int endNumber = endValue < 0 ? -endValue : endValue;
+
+int sumR = 0;
+int sumL = 0;
+if (startValue >= 0)
+{
+    for (int i = startValue; i > 1; i--)
+    {
+        int k;
+        for (int n=2;n<startValue;n++)
+        {
+            k = i;
+            k = ((startValue % n) == 0) && (i!= n) ? k = 0 : k;
+            sumR += k;
+           
+        }
+                Console.Write("i= " + i + "  ");
+    Console.WriteLine("sum= " + sumR);
+    }
+}
+
+else
+{
+    for (int i = startValue; i < -1; i++)
+    {
+        for (int n = 2; n > startValue; n++)
+        {
+            i = (startValue % n == 0) && (n != i) ? i = 0 : i;
+        }
+        sumR += i;
+        Console.Write("i= " + i + "  ");
+        Console.WriteLine("sum= " + sumR);
+    }
+    
+}
+ sumR = sumR < 0 ? -sumR : sumR;
+
+if (endValue >= 0)
+{
+    for (int i = endValue; i > 1; i--)
+    {
+        for (int n = 2; n < endValue; n++)
+        {
+            i = (startValue % n == 0) && (n != i) ? i = 0 : i;
+        }
+        sumL += i;
+        Console.Write("i= " + i + "  ");
+        Console.WriteLine("sum= " + sumL);
+    }
+}
+
+else
+{
+    for (int i = endValue; i < -1; i++)
+    {
+        int k;
+        for (int n = -2; n < endValue; n--)
+        {
+            k = i;
+            k = (endValue % n == 0) && (n != i) ? k = 0 : k;
+            sumL += k;
+        }
+        Console.Write("i= " + i + "  ");
+        Console.WriteLine("sum= " + sumL);
+    }
+
+}
+sumL = sumL < 0 ? -sumL : sumL;
+int sum = sumL + sumR;
+Console.WriteLine();
+
+Console.WriteLine("sum = " + sum);
---
+++ tests/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+int startValue = int.Parse(Console.ReadLine());
+int k;
+int sumR=0;
+int i = 10;
+for (int n = 2; n < startValue; n++)
+{
+    k = i;
+    k = ((startValue % n) == 0) && (i != n) ? k = 0 : k;
+    sumR += k;
+    Console.Write("i= " + i + "  ");
+    Console.Write("k= " + k + "  ");
+    Console.WriteLine("sum= " + sumR);
+}
+
