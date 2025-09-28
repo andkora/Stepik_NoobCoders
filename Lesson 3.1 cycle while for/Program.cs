664a6fd5e5db2d034d994f9ae2a59af41887d0b5
Andrey Korochkin <charpdeveloper@outlook.com>
2025-09-28 23:16:30 +0300

    Решения 6 и 7 задач

--- Lesson 3.1 cycle while for/Program.cs
+++ Lesson 3.1 cycle while for/Program.cs
@@ -84,6 +84,7 @@ for (int i = 2; i <= num; i++)
 //Задача 5
 /* Напишите программу, которая считает сумму всех простых чисел в определенном диапазоне
  */
+/*
 int startValue = int.Parse(Console.ReadLine());
 int endValue = int.Parse(Console.ReadLine());
 
@@ -161,3 +162,108 @@ int sum = sumL + sumR;
 Console.WriteLine();
 
 Console.WriteLine("sum = " + sum);
+*/
+/*
+ *  
+ Задача 6, Шаг 13 по курсу из Stepik
+В консоль вводятся два числа. 
+В обоих числах есть одна общая цифра.
+ Выведите в консоль цифру, которая есть в
+  обоих числах через пробел столько раз,
+  сколько раз она встречается в первом числе.
+  */
+/*
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+string firstDigits = firstNumber.ToString();
+string secondDigits = secondNumber.ToString();
+
+char commonDigit = ' ';
+
+// 2. Проверяем каждую цифру
+for (int i = 0; i < firstDigits.Length; i++)
+{
+
+
+    for (int j = 0; j < secondDigits.Length; j++)
+    {
+        if (firstDigits[i] == secondDigits[j])
+        {
+            commonDigit = firstDigits[i];
+            break;
+        }
+
+
+    }
+
+    if (commonDigit != ' ') break; // Нашли общую цифру, выходим
+
+}
+// 2. Считаем сколько раз эта цифра встречается в первом числе
+if (commonDigit != ' ')
+{
+    int count = 0;
+    for (int i = 0; i < firstDigits.Length; i++)
+    {
+        if (firstDigits[i] == commonDigit)
+        {
+            count++;
+        }
+    }
+    // 3. Выводим цифру count раз
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(commonDigit + " ");
+    }
+}
+
+
+    }
+
+}
+ */
+/*Задача 7 Шаг 14
+
+Гипотеза Сиракуз утверждает, что любое натуральное число сводится к единице
+    в результате повторения следующих действий над самим числом и результатами этих действий:
+
+Если число четное, то его следует разделить на 2
+
+Если нечетное, то умножить его на 3, а затем прибавить 1
+
+В попытке доказать эту гипотезу, Лотар Коллатц написал программу, 
+которая должна выводить в консоль количество действий, которые необходимо совершить над введенным в консоль числом,
+прежде чем оно сведется к единице.
+
+1. 5 - это нечётное число, значит 5 * 3 + 1 = 16
+2. 16 - чётное, 16 / 2 = 8
+3. 8 - чётное, 8 / 2 = 4
+4. 4 - чётное, 4 / 2 = 2
+5. 2 - чётное, 2 / 2 = 1
+
+Итого потребовалось 5 действий
+
+ int number = int.Parse(Console.ReadLine());
+        int count = 0;
+        if (number != 1)
+        {
+            do
+            {
+
+                if (number % 2 == 0)
+                { number /= 2; }
+                else
+                {
+                    number = (number * 3) + 1;
+                }
+                count++;
+                Console.Write($"count = {count}  ");
+                Console.WriteLine($"number = {number}");
+            }
+            while (number > 1);
+        }
+        Console.WriteLine($"Количество необходимых действий над числом - {count}");
+*/
+//Выгрузка на гит 6 и 7 задачи
